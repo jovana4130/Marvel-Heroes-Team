1b0db542858438d5a28aaf13f5bdaa9e3fdc7975
Jovana Vucetic <jovanavucetic4130@gmail.com>
2023-03-18 11:03:54 +0100

    Files update

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import React from 'react';
 import './App.css';
-import { MainPage } from './components/MainPage/MainPage';
+import { MainPage } from './components/MainPage';
 import { Route } from 'react-router-dom';
-import { Footer } from './components/Footer/Footer';
+import { Footer } from './components/Footer';
 
 function App() {
   return (
     <div className='container'>
-      <div className='content'>
-        <Switch>
+        
           <Route exact path='/' component={ MainPage } />
           <Route exact path='/infopage/:id' component={ InfoPage } />
-        </Switch>
-      </div>
+        
       <Footer />
     </div>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import App from './App';
 import { HashRouter } from 'react-router-dom';
 
 
-ReactDOM.render(
-  <HashRouter>
-    <App />
-  </HashRouter>,
-  document.getElementById('root')
-);
+    ReactDOM.render(
+    <HashRouter>
+        <App />
+    </HashRouter>,
+    document.getElementById('root')
+    );
