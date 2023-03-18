c978500cbb2e2331bd0b4f2fced7e078d8ba5030
Jovana Vucetic <jovanavucetic4130@gmail.com>
2023-03-18 10:48:01 +0100

    File organisation

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+import { MainPage } from './components/MainPage/MainPage';
+import { Route } from 'react-router-dom';
+import { InfoPage } from './components/InfoPage/InfoPage';
+import { Footer } from './components/Footer/Footer';
+
+function App() {
+  return (
+    <div className='container'>
+      <div className='content'>
+        <Switch>
+          <Route exact path='/' component={ MainPage } />
+          <Route exact path='/infopage/:id' component={ InfoPage } />
+        </Switch>
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
