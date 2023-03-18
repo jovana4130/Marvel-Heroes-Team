4d04201abb0c8140660cead1a941c6b6334b8fe9
Jovana Vucetic <jovanavucetic4130@gmail.com>
2023-03-18 16:27:47 +0100

    useEffect and useState

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -1,21 +1,37 @@
 import React from 'react';
 import { Header } from './Header/Header';
-import './MainPage/MainPage';
+import './MainPage/MainPage.css';
+import { useEffect, useState } from 'react';
 
+export const MainPage = () => {
+    const [character, setCharacters] = useState([]);
+    const addOnList = () => {
+        
+    }
 
-export const MainPage = () => {}
+    useEffect(() => {
+        fetch('http://gateway.marvel.com/v1/public/characters?apikey=____42c944f16bc51826eb97319ae90f78b6')
+        .then(data => data.json())
+        .then( 
+            res => {
+                console.log(res.data.results)
+                setCharacters(res.data.results)
+            }
+        )
+    }, [])
+}
 
 return (
     <div className='box'>
-        {character.map((character) => (
-            <div className='character' key={character.id}>
-                <div>{character.name}</div>
+        { character.map((character) => (
+            <div className='character' key={ character.id }>
+                <div>{ character.name }</div>
                 <div className='image'>
-                    <img src={character.thumbnail.path + '.' + character.thumbnail.extension} alt={character.name} />
+                    <img src={ character.thumbnail.path + '.' + character.thumbnail.extension } alt={ character.name } />
                 </div>
             <div className='button-box'>
-                <button>INFO</button>
-                <button>ADD</button>
+                <button>Info</button>
+                <button>Add</button>
             </div>
         </div> 
         ))}
