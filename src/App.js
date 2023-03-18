ba592966266408b7b39e8a74970720c966b9380c
Jovana Vucetic <jovanavucetic4130@gmail.com>
2023-03-18 12:17:59 +0100

    Search input

--- src/App.js
+++ src/App.js
@@ -4,15 +4,19 @@ import { Header } from './components/Header/Header'
 import { MainPage } from './components/MainPage/MainPage';
 import { Route } from 'react-router-dom';
 import { Footer } from './components/Footer/Footer';
+import { SearchInput } from './components/SearchInput'
 
 function App() {
   return (
     <div className='App'>
-        <Header>Hero Team</Header>
+        <Header></Header>
+          <div className='search-input'>
+            <SearchInput></SearchInput>
+            </div>
             <MainPage>
                 <Route exact path='/' component={ MainPage } />
             </MainPage>
-        <Footer />
+        <Footer></Footer>
     </div>
   );
 };
