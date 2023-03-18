c02cbe448ecfc64ef31d348638d54256fa7f077a
Jovana Vucetic <jovanavucetic4130@gmail.com>
2023-03-18 16:58:09 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
 import './App.css';
-import { Header } from './components/Header/Header'
-import { MainPage } from './components/MainPage/MainPage';
+import { Header } from './components/Header/Header.js'
+import { MainPage } from './components/MainPage/MainPage.js';
 //import { Route } from 'react-router-dom';
-import { Footer } from './components/Footer/Footer';
+import { Footer } from './components/Footer/Footer.js';
 //import { SearchInput } from './components/SearchInput'
 
 function App() {
-  /*return (
+  return (
     <div className='App'>
-        <Header></Header>
-          <div className='search-input'>
-            <SearchInput></SearchInput>
-            </div>
-            <MainPage>
-                <Route exact path='/' component={ MainPage } />
-            </MainPage>
+        <Header>Hero Team</Header>
+          <div className='search-input'></div>
+            <MainPage></MainPage>
         <Footer></Footer>
     </div>
-  );*/
+  );
 };
 
 export default App;
+
+
+
+//<SearchInput></SearchInput>
+//<Route exact path='/' component={ MainPage } />
--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import App from './App.js';
+//import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    
-        <BrowserRouter>
-            <StrictMode>
-                <App />
-            </StrictMode>
-        </BrowserRouter>,
-    
+        
+
     document.getElementById('root')
     );
+
