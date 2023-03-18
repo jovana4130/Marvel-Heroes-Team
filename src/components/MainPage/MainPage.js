3834eb8b0998335d169058efd7d0ea4f861ba72a
Jovana Vucetic <jovanavucetic4130@gmail.com>
2023-03-18 15:46:35 +0100

    MainPage return

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -5,7 +5,22 @@ import './MainPage/MainPage';
 
 export const MainPage = () => {}
 
-
+return (
+    <div className='box'>
+        {character.map((character) => (
+            <div className='character' key={character.id}>
+                <div>{character.name}</div>
+                <div className='image'>
+                    <img src={character.thumbnail.path + '.' + character.thumbnail.extension} alt={character.name} />
+                </div>
+            <div className='button-box'>
+                <button>INFO</button>
+                <button>ADD</button>
+            </div>
+        </div> 
+        ))}
+    </div>
+)
 
 export { MainPage };
 
