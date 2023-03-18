723ba4cdc39c5d8432702c69664a98656ad78791
Jovana Vucetic <jovanavucetic4130@gmail.com>
2023-03-18 11:32:12 +0100

    Update

--- src/index.js
+++ src/index.js
@@ -2,12 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 
-    ReactDOM.render(
-    <HashRouter>
-        <App />
-    </HashRouter>,
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    
+        <BrowserRouter>
+            <StrictMode>
+                <App />
+            </StrictMode>
+        </BrowserRouter>,
+    
     document.getElementById('root')
     );
