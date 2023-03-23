cc48db445f778a373d8557ad2061e21ce86406d4
Jovana Vucetic <jovanavucetic4130@gmail.com>
2023-03-23 18:39:10 +0100

    Update

--- src/components/Card/card.js
+++ src/components/Card/card.js
@@ -2,7 +2,7 @@ import React from 'react';
 import style from './Card.css';
 /*import { Link } from 'react-router-dom';*/
 
-export const Card = ({ heroes, addToMyTeam }) => {
+/*export const Card = ({ heroes, addToMyTeam }) => {
     return (
         <div>
             {heroes.map(hero => 
@@ -15,5 +15,5 @@ export const Card = ({ heroes, addToMyTeam }) => {
                 />)}
         </div>
     );
-};
+};*/
 
